70110bb171eba538aff1a4769c0066ee86dfa0ca
Miles Davis <Miles Davis@DESKTOP-SFJDIF0.home>
2022-03-20 01:51:53 -0400

    Implemented hashMap for multi-panel count and completely finished tables to display the single panel count, multi-panel count, and the total count.

--- Canvas Freaks Automation/src/GUI.java
+++ Canvas Freaks Automation/src/GUI.java
@@ -16,6 +16,7 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
@@ -35,10 +36,11 @@ import org.apache.commons.lang3.ArrayUtils;
 public class GUI extends JFrame implements ActionListener {
 	
 	//Define dimensions array
-	String [] dimensionsArray = {
+	static String [] dimensionsArray = {
 			"8x8", "10x8", "10x10", "12x8", "12x9", "12x12", "14x11", "14x12", "16x8", "16x12", "16x16", "18x12", "20x10", "20x16", "20x20", "24x12", "24x16",  "24x18",  "24x24",  
 			"28x20",  "28x28",  "30x15",  "30x20",  "30x30",  "32x16",  "32x24",  "32x32",  "36x24",  "36x36",  "40x20", "40x26", "40x28", "40x30", "40x40", "48x24", "48x32", "48x36", "50x40", "50x50", "60x20", "60x30", "60x40"
 			};
+	static String [] multiSizes = {"5XLP", "5MXLP", "5XLSP", "3XLP", "4XLP", "5LP", "5MLP", "5LSP", "3LP", "4LP", "5MP", "5MSP", "3MP", "4MP"};
 	
 	//Initialize buttons
 	JButton multiButton;
@@ -51,7 +53,7 @@ public class GUI extends JFrame implements ActionListener {
 	
 	//Initialize HashMap used for single panel count
 	private static HashMap<String, int[]> singlePanelCount = new HashMap<String, int[]>();
-
+	private static HashMap<String, Integer> multiPanelCount = new HashMap<String, Integer>();
 	
 	//Initialize global sub-folders to be null
 	static File largeFolderThree = null;
@@ -63,27 +65,6 @@ public class GUI extends JFrame implements ActionListener {
 	static File extraLargeFolderThree = null;
 	static File extraLargeFolderOneAndFive = null;
 	static File extraLargeFolderTwoAndFour = null;
-	
-
-	//Initialize global multi-panel total count and count for each set type
-	static int totalMultiCount;
-	//XL
-	static int fiveExtraLargeCount = 0;
-	static int fiveMegaExtraLargeCount = 0;
-	static int fiveStaggeredExtraLargeCount = 0;
-	static int fourExtraLargeCount = 0;
-	static int threeExtraLargeCount = 0;		
-	//L
-	static int fiveLargeCount = 0;
-	static int fiveMegaLargeCount = 0;
-	static int fiveStaggeredLargeCount = 0;
-	static int fourLargeCount = 0;
-	static int threeLargeCount = 0;
-	//M
-	static int fiveMediumCount = 0;
-	static int fiveMediumStaggeredCount = 0;
-	static int fourMediumCount = 0;
-	static int threeMediumCount = 0;
 	
 	//Initialize floating frame count
 	int FLCount;
@@ -128,6 +109,7 @@ public class GUI extends JFrame implements ActionListener {
 		this.add(mergeButton);
 		this.setSize(500, 300);
 		this.setVisible(true);
+		
 	}
 	
 	
@@ -244,26 +226,30 @@ public class GUI extends JFrame implements ActionListener {
 					String [] parts = filePath.split("\\\\");
 	                String fileName = parts[parts.length-1];
 	                String folderName = parts[parts.length-2];
-	              
-	                if (!singlePanelCount.containsKey(folderName)) {
-	                    //If it doesn't contain the key already, make a new array of size 3 for that key
-	                    singlePanelCount.put(folderName, new int[]{0,0,0});
-	                    
-	                } if (Character.isDigit(folderName.charAt(0))) {
-		                //filter logic to add counts
-	                	int[] intTemp = singlePanelCount.get(folderName);
-		               	if (fileName.contains("FL")) {
-		               		intTemp[2]++;
-		                } else if (fileName.contains("1.5")) {
-		                    intTemp[1]++;
-		               	} else {
-		               		intTemp[0]++;
+	                if (!filePath.contains("Multi")) {
+		                if (!singlePanelCount.containsKey(folderName)) {
+		                    //If it doesn't contain the key already, make a new array of size 3 for that key
+		                    singlePanelCount.put(folderName, new int[]{0,0,0});
+		                    
+		                } if (Character.isDigit(folderName.charAt(0))) {
+			                //filter logic to add counts
+		                	int[] intTemp = singlePanelCount.get(folderName);
+			               	if (fileName.contains("FL")) {
+			               		intTemp[2]++;
+			                } else if (fileName.contains("1.5")) {
+			                    intTemp[1]++;
+			               	} else {
+			               		intTemp[0]++;
+			                }
+			               	singlePanelCount.put(folderName, intTemp);
 		                }
-		               	singlePanelCount.put(folderName, intTemp);
+	                } else {
+	                	getMultiCount(filePath);
 	                }
 				}
+				
 				displayCount(chosenFolder);
-			
+				
 			}
 			
 		//If "Organize Multi Files" is selected	
@@ -293,29 +279,7 @@ public class GUI extends JFrame implements ActionListener {
 				}
 					//Create count.txt and display count for each size and set
 				
-					Path path = Paths.get(chosenFolder.toString() + "\\count.txt"); //creates Path instance  
-					try  {  
-						totalMultiCount = fiveExtraLargeCount + fiveMegaExtraLargeCount + fiveStaggeredExtraLargeCount +  fourExtraLargeCount + threeExtraLargeCount + fiveLargeCount + fiveMegaLargeCount + fiveStaggeredLargeCount + fourLargeCount + threeLargeCount + fiveMediumCount +  fiveMediumStaggeredCount + fourMediumCount + threeMediumCount;   
-						Path p = Files.createFile(path);     //creates file at specified location  
-						List<String> lines = Arrays.asList(
-						//Displaying count for XL
-						"Count (Total: " + totalMultiCount + " panels" + ")", " ", "Extra Large", " ", "5XLP: " + fiveExtraLargeCount + " panels " + "(" + fiveExtraLargeCount/5 + " set(s))", "5XLSP: " + fiveStaggeredExtraLargeCount + " panels " + "(" + fiveStaggeredExtraLargeCount/5 + " set(s))",
-						"5MXLP: " + fiveMegaExtraLargeCount + " panels " + "(" + fiveMegaExtraLargeCount/5 + " set(s))", "4XLP: " + fourExtraLargeCount + " panels " + "(" + fourExtraLargeCount/4 + " set(s))",
-						"3XLP: " + threeExtraLargeCount + " panels " + "(" + threeExtraLargeCount/3 + " set(s))", " ",
-						//Displaying count for Large
-						"Large", " ", "5LP: " + fiveLargeCount + " panels " + "(" + fiveLargeCount/5 + " set(s))", "5LSP: " + fiveStaggeredLargeCount + " panels " + "(" + fiveStaggeredLargeCount/5 + " set(s))",
-						"5MLP: " + fiveMegaLargeCount + " panels " + "(" + fiveMegaLargeCount/5 + " set(s))", "4LP: " + fourLargeCount + " panels " + "(" + fourLargeCount/4 + " set(s))",
-						"3LP: " + threeLargeCount + " panels " + "(" + threeLargeCount/3 + " set(s))", " ",
-						//Displaying count for Medium
-						"Medium", " ", "5MP: " + fiveMediumCount + " panels " + "(" + fiveMediumCount/5 + " set(s))", "5MSP: " + fiveMediumStaggeredCount + " panels " + "(" + fiveMediumStaggeredCount/5 + " set(s))",
-						"4MP: " + fourMediumCount + " panels " + "(" + fourMediumCount/4 + " set(s))",
-						"3MP: " + threeMediumCount + " panels " + "(" + threeMediumCount/3 + " set(s))"
-						);
-						Files.write(p, lines, StandardCharsets.UTF_8,  StandardOpenOption.APPEND);
-						System.out.println("File Created at Path: "+ p);     
-					} catch (IOException e3) {   
-						e3.printStackTrace(); 
-					}  
+					 
 			} else {
 				System.out.println("Open command canceled");
 			}
@@ -340,6 +304,8 @@ public class GUI extends JFrame implements ActionListener {
 	}
 	
 	 public static void displayCount(File folder) {
+		 	//Create frame and models for table
+		 
 		    JFrame frame = new JFrame("Count");
 		    
 		    DefaultTableModel tableModel = new DefaultTableModel();
@@ -347,15 +313,23 @@ public class GUI extends JFrame implements ActionListener {
 		    DefaultTableModel totalCountModel = new DefaultTableModel();
 		    
 		    JTable tableSingle = new JTable(tableModel);
+		    JTable tableMulti = new JTable(tableMultiModel);
 		    JTable tableTotalCount = new JTable(totalCountModel);
 		    
 		    JTableHeader header1 = tableSingle.getTableHeader();
 		    header1.setBackground(Color.yellow);
 		    JTableHeader header2 = tableTotalCount.getTableHeader();
 		    header2.setBackground(Color.yellow);
+		    JTableHeader header3 = tableMulti.getTableHeader();
+		    header3.setBackground(Color.yellow);
 		    
+		    tableMulti.setRowHeight(70);
 		    tableTotalCount.setRowHeight(70);
 		    tableSingle.setRowHeight(70);
+		    
+		    tableMultiModel.addColumn("Multi Panel Sizes");
+		    tableMultiModel.addColumn("Quantities Of Panels");
+		    
 		    
 		    tableModel.addColumn("Dimension");
 		    tableModel.addColumn("0.75");
@@ -370,21 +344,30 @@ public class GUI extends JFrame implements ActionListener {
 		    int firstSizeCount = 0;
 		    int secondSizeCount = 0;
 		    int thirdSizeCount = 0;
+		    int totalMultiCount = 0;
 		    
 		    tableSingle.getTableHeader().setResizingAllowed(true);
 		    
-		    int totalCount = 0;
+		    int totalSingleCount = 0;
 		    int zeroPointSevenFiveCount = 0;
 		    int onePointFiveCount = 0;
 		    int FLCount = 0;
-		    for (String key : singlePanelCount.keySet()) {
+		    int totalCount; 
+		    
+		    
+		    //Sort the keySet so the sizes are in order
+		    List<String> sortedKeySet = new ArrayList<>(singlePanelCount.keySet());
+		    Collections.sort(sortedKeySet);
+		    
+		    //Loop through the sortedKeySet and add up all the panels with similar thickness then add rows for each size and thickness'
+		    for (String key : sortedKeySet) {
 			      int[] temp = singlePanelCount.get(key);
 			      int i;
 			      for (i = 0; i < temp.length; i++) {
 			        firstSizeCount = temp[0];
 			        secondSizeCount = temp[1];
 			        thirdSizeCount = temp[2];
-			        totalCount += temp[i];
+			        totalSingleCount += temp[i];
 			      } 
 			      for (i = 0; i < temp.length; i += 3)
 			        zeroPointSevenFiveCount += temp[i]; 
@@ -392,82 +375,78 @@ public class GUI extends JFrame implements ActionListener {
 			        onePointFiveCount += temp[i]; 
 			      for (i = 2; i < temp.length; i += 3)
 			        FLCount += temp[i]; 
+			      
 			      tableModel.addRow(new Object[] { key, firstSizeCount, secondSizeCount, thirdSizeCount});
 			    } 
+		    
+		    //Loop through multiPanelCOunt and add row with each size and amount
+		    for (String key: multiPanelCount.keySet()) {
+				   int tmp = multiPanelCount.get(key);
+				   totalMultiCount+=tmp;
+				   tableMultiModel.addRow(new Object[] { key, tmp });
+				   
+			   }
+		    
+		    totalCount = totalSingleCount + totalMultiCount;
 		    totalCountModel.addRow(new Object[] { zeroPointSevenFiveCount, onePointFiveCount, FLCount, totalCount});
 		    
 		    Font myFont = new Font("Calibiri", 0, 14);
-		    JSplitPane splitPane = new JSplitPane(1);
+		    
+		    //Makes the tables fit nicely in the frame
+		    
+		    JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, tableMulti, tableSingle);
+		    JSplitPane splitPane2 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, splitPane, tableTotalCount);
+		    
 		    splitPane.add(new JScrollPane(tableSingle));
-		    splitPane.add(new JScrollPane(tableTotalCount));
+		    splitPane.add(new JScrollPane(tableMulti));
+		    splitPane2.add(new JScrollPane(tableTotalCount));
+		    
+		    
 		    tableSingle.setFont(myFont);
 		    tableTotalCount.setFont(myFont);
 		    
 		    frame.setDefaultCloseOperation(3);
-		    frame.add(splitPane);
+		    frame.add(splitPane2);
 		    frame.setSize(800, 300);
 		    frame.pack();
 		    frame.setVisible(true);
+		    
 		  }
 	
 	public static void getMultiCount(String filePath) {
-		
-		if (filePath.contains("Extra Large")) {
-			if (filePath.contains("5XLP1")) 
-				fiveExtraLargeCount+=5;
-			else if (filePath.contains("5MXLP1")) 
-				fiveMegaExtraLargeCount+=5;
-			else if (filePath.contains("5XLSP1"))
-				fiveStaggeredExtraLargeCount+=5;
-			else if (filePath.contains("3XLP1")) 
-				threeExtraLargeCount+=3;
-			else if (filePath.contains("4XLP1"))
-				fourExtraLargeCount+=4;				
-		//Large count for each set
-			
-		} else if (filePath.contains("Large") && !filePath.contains("Extra Large")) {
-			if (filePath.contains("5LP1")) 
-				fiveLargeCount+=5;
-			else if (filePath.contains("5MLP1")) 
-				fiveMegaLargeCount+=5;
-			else if (filePath.contains("5LSP1")) 
-				fiveStaggeredLargeCount+=5;
-			else if (filePath.contains("3LP1"))
-				threeLargeCount+=3;
-			else if (filePath.contains("4LP1"))
-				fourLargeCount+=4;		
-		//Medium count for each set
-			
-		} else if (filePath.contains("Medium")){
-			if (filePath.contains("5MP1")) 
-				fiveMediumCount+=5;
-			else if (filePath.contains("5MSP1")) 
-				fiveMediumStaggeredCount+=5;
-			else if (filePath.contains("3MP1")) 
-				threeMediumCount+=3;
-			else if (filePath.contains("4MP1")) 
-				fourMediumCount+=4;		
+			//Loop through array of possible multi panel sizes, if the hashMap doesn't contain a size then put that size in hashMap with value of 0.
+			//Then if a file contains a size in the multiSizes array (key) then add 1 to the amount of files (value)
+            if (filePath.contains("Multi")) {
+            	for (String size: multiSizes) {
+            		if (!multiPanelCount.containsKey(size)) {
+            			multiPanelCount.put(size, 0);
+            		}
+            		int tmp = multiPanelCount.get(size);
+                 	if (filePath.contains(size)) {
+                 		tmp++;
+	            	}
+                 	multiPanelCount.put(size, tmp);
+                }
+            	
+            }
 		}
-	}
-	
-	
-	
+
 	public static void sortFiles(File f, String filePath) {
 		
-		if (Stream.of("5LP3", "4LP2", "4LP3", "5LSP3", "5MLP1","5MLP2", "5MLP3", "5MLP4", "5MLP5").anyMatch(filePath::contains)) {
+		if (Stream.of("5LP3", "4LP2", "4LP3", "5LSP3", "5MLP1","5MLP2", "5MLP3", "5MLP4", "5MLP5", "5LP(3)", "4LP(2)", "4LP(3)", "5LSP(3)", "5MLP(1)","5MLP(2)", "5MLP(3)", "5MLP(4)", "5MLP(5").anyMatch(filePath::contains)) {
 			try {
 				FileUtils.moveFileToDirectory(f, largeFolderThree, false);
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
-		} else if (Stream.of("4LP1", "4LP4", "3LP1", "3LP2", "3LP3","5LSP1", "5LSP2", "5LSP4", "5LSP5", "5LP2", "5LP4").anyMatch(filePath::contains)) {
+		} else if (Stream.of("4LP1", "4LP4", "3LP1", "3LP2", "3LP3","5LSP1", "5LSP2", "5LSP4", "5LSP5", "5LP2", "5LP4", "4LP(1)", "4LP(4)", "3LP(1)", "3LP(2)", "3LP(3)","5LSP(1)", "5LSP(2)", "5LSP(4)", "5LSP(5)", "5LP(2)", "5LP(4)").anyMatch(filePath::contains)) {
 				try {
 					FileUtils.moveFileToDirectory(f, largeFolderTwoAndFour, false);
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
 			
-		} else if (Stream.of("5LP1", "5LP5").anyMatch(filePath::contains)) {
+		} else if (Stream.of("5LP1", "5LP5", "5LP(1)", "5LP(5)").anyMatch(filePath::contains)) {
 				try {
 					FileUtils.moveFileToDirectory(f, largeFolderOneAndFive, false);
 				} catch (IOException e1) {
@@ -476,20 +455,20 @@ public class GUI extends JFrame implements ActionListener {
 				
 		//Extra Large folder file to folders sorting logic
 				
-		} else if (Stream.of("5XLP3", "4XLP2", "4XLP3", "5XLSP3", "5MXLP1","5MXLP2", "5MXLP3", "5MXLP4", "5MXLP5").anyMatch(filePath::contains)) {
+		} else if (Stream.of("5XLP3", "4XLP2", "4XLP3", "5XLSP3", "5MXLP1","5MXLP2", "5MXLP3", "5MXLP4", "5MXLP5", "5XLP(3)", "4XLP(2)", "4XLP(3)", "5XLSP(3)", "5MXLP(1)","5MXLP(2)", "5MXLP(3)", "5MXLP(4)", "5MXLP(5)").anyMatch(filePath::contains)) {
 			try {
 				FileUtils.moveFileToDirectory(f, extraLargeFolderThree, false);
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
-		} else if (Stream.of("4XLP1", "4XLP4", "3XLP1", "3XLP2", "3XLP3","5XLSP1", "5XLSP2", "5XLSP4", "5XLSP5", "5XLP2", "5XLP4").anyMatch(filePath::contains)) {
+		} else if (Stream.of("4XLP1", "4XLP4", "3XLP1", "3XLP2", "3XLP3","5XLSP1", "5XLSP2", "5XLSP4", "5XLSP5", "5XLP2", "5XLP4", "4XLP(1)", "4XLP(4)", "3XLP(1)", "3XLP(2)", "3XLP(3)","5XLSP(1)", "5XLSP(2)", "5XLSP(4)", "5XLSP(5)", "5XLP(2)", "5XLP(4)").anyMatch(filePath::contains)) {
 				try {
 					FileUtils.moveFileToDirectory(f, extraLargeFolderTwoAndFour, false);
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
 			
-		} else if (Stream.of("5XLP1", "5XLP5").anyMatch(filePath::contains)) {
+		} else if (Stream.of("5XLP1", "5XLP5", "5XLP(1)", "5XLP(5)").anyMatch(filePath::contains)) {
 				try {
 					FileUtils.moveFileToDirectory(f, extraLargeFolderOneAndFive, false);
 				} catch (IOException e1) {
@@ -498,21 +477,21 @@ public class GUI extends JFrame implements ActionListener {
 	
 		//Medium Folder file to folders sorting logic (no MEGA MEDIUM)
 				
-		} else if (Stream.of("5MP3", "4MP2", "4MP3", "5MSP3").anyMatch(filePath::contains)) {
+		} else if (Stream.of("5MP3", "4MP2", "4MP3", "5MSP3", "5MP(3)", "4MP(2)", "4MP(3)", "5MSP(3)").anyMatch(filePath::contains)) {
 			try {
 				FileUtils.moveFileToDirectory(f, mediumFolderThree, false);
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
 			
-		} else if (Stream.of("4MP1", "4MP4", "3MP1", "3MP2", "3MP3","5MSP1", "5MSP2", "5MSP4", "5MSP5", "5MP2", "5MP4").anyMatch(filePath::contains)) {
+		} else if (Stream.of("4MP1", "4MP4", "3MP1", "3MP2", "3MP3","5MSP1", "5MSP2", "5MSP4", "5MSP5", "5MP2", "5MP4", "4MP(1)", "4MP(4)", "3MP(1)", "3MP(2)", "3MP(3)", "5MSP(1)", "5MSP(2)", "5MSP(4)", "5MSP(5)", "5MP(2)", "5MP(4)").anyMatch(filePath::contains)) {
 				try {
 					FileUtils.moveFileToDirectory(f, mediumFolderTwoAndFour, false);
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
 			
-		} else if (Stream.of("5MP1", "5MP5").anyMatch(filePath::contains)) {
+		} else if (Stream.of("5MP1", "5MP5", "5MP(1)", "5MP(5)").anyMatch(filePath::contains)) {
 				try {
 					FileUtils.moveFileToDirectory(f, mediumFolderOneAndFive, false);
 				} catch (IOException e1) {
